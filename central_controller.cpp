9bd90ab884552dd2d049a99347729b9899ef38bd
Sach <sachsiri@live.co.uk>
2019-02-16 21:24:08 +1100

    Added initial debugging code

--- central_controller.cpp
+++ central_controller.cpp
@@ -4,13 +4,19 @@
 using namespace std;
 
 //  Global variables
+int ball_position_x = 0;
+int ball_position_y = 0;
 
 //  Global functions
-void ball_function {
-    cout << "Start of ball_function" << endl;   //Debugging purposes only
+void ball_function() {
+    cout << "Start of ball_function" << endl;       //Debugging purposes only
+    cout << "Position of the ball" << endl;         //Debugging purposes only
+    cout << "   X = " << ball_position_x << endl;   //Debugging purposes only
+    cout << "   Y = " << ball_position_y << endl;   //Debugging purposes only
 }
 
 int main() {
-    cout << "Hello wordl!" << endl;
+    cout << "Hello world!" << endl;
+    ball_function();
     return 0;
 }
