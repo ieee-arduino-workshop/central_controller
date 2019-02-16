0ed8bd384a04d7a759d94305dd71fa21bc899d12
Sach <sachsiri@live.co.uk>
2019-02-16 20:37:36 +1100

    Created branch for ball physics

--- central_controller.cpp
+++ central_controller.cpp
@@ -1,7 +1,14 @@
-// Develop branch - test code
+//  Develop branch - test code
 #include <iostream>
 
 using namespace std;
+
+//  Global variables
+
+//  Global functions
+void ball_function {
+    cout << "Start of ball_function" << endl;   //Debugging purposes only
+}
 
 int main() {
     cout << "Hello wordl!" << endl;
