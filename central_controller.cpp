0b5d61c9c1eb257cca4789e612098dd471909fef
Sach <sachsiri@live.co.uk>
2019-02-17 17:46:47 +1100

    Added intial algorithm to prevent the ball from going beyond the pitch

--- central_controller.cpp
+++ central_controller.cpp
@@ -13,6 +13,10 @@ int ball_position_y_previous = 0;
 // Ball user input variables
 int ball_user_x = 0;
 int ball_user_y = 0;
+// Soccer field dimension variables - These values will change in the future
+int pitch_x = 1500;
+int pitch_y = 1000;
+
 
 //  ------Global functions------
 void ball_function(int x, int y) {
@@ -21,10 +25,17 @@ void ball_function(int x, int y) {
     cout << "Position of the ball : Previous" << endl;      //Debugging purposes only
     cout << "   X = " << ball_position_x_previous << endl;  //Debugging purposes only
     cout << "   Y = " << ball_position_y_previous << endl;  //Debugging purposes only
+
+    // Checking if the ball is at the boundaries of the pitch
+    if (x > pitch_x || x == pitch_x) {      // Checking if the ball has exceed the x-axis boundaries
+        ball_position_x_now = pitch_x;      // Preventing the ball from exceeding the boundaries by keeping the ball at the boundary
+    } if (y > pitch_y || y == pitch_y) {    // Checking if the ball has exceed the x-axis boundaries
+        ball_position_y_now = pitch_y;      // Preventing the ball from exceeding the boundaries by keeping the ball at the boundary
+    } else {                                // If the ball doesn't exceed the boundaries of the pitch
+        ball_position_x_now = x;
+        ball_position_y_now = y;
+    }
     
-    // The ball's current position
-    ball_position_x_now = x;
-    ball_position_y_now = y;
     // The ball's previous position
     ball_position_x_previous = ball_position_x_now;
     ball_position_y_previous = ball_position_y_now;
