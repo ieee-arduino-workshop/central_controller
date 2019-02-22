e557222bc688ce572d1c8aaa849c09b1ede79ab6
lehai730 <lehai730@gmail.com>
2019-02-22 18:06:52 +1100

    added a prototype ball that stuck to the player movements

--- central_controller.cpp
+++ central_controller.cpp
@@ -1,4 +1,3 @@
-//player test code - Hai
 #include <iostream>
 #include <conio.h>
 #include <Windows.h>
@@ -30,22 +29,6 @@ public:
 	{
 		direction = d;
 	}
-	void move_player() {
-		switch (direction) {
-		case STOP:
-			break;
-		case LEFT:
-			x--;
-		case RIGHT:
-			x++;
-		case UP:
-			y--;
-		case DOWN:
-			y++;
-		default:
-			break;
-		}
-	}
 	inline int getX() { return x; } //public get x and y function (use inline to replace those function definition wherever those are being called)
 	inline int getY() { return y; }
 	inline void moveUp() { y--; }
@@ -55,18 +38,44 @@ public:
 	inline int getDirection() { return direction; } //public get current direction
 };
 
+class Ball {
+private:
+	int x, y;
+	int oriX, oriY;
+public:
+	Ball(int posX, int posY) {
+		oriX = posX;
+		oriY = posY;
+		x = posX;
+		y = posY;
+	}
+	void Reset() {
+		x = oriX; y = oriY;
+	}
+	inline int getX() { return x; } //public get x and y function (use inline to replace those function definition wherever those are being called)
+	inline int getY() { return y; }
+	inline void setX(int newX) { x = newX; }
+	inline void setY(int newY) { y = newY; }
+	inline void moveUp() { y--; }
+	inline void moveDown() { y++; }
+	inline void moveLeft() { x--; }
+	inline void moveRight() { x++; }
+};
+
 class game_manager {
 private:
 	int width, height;
 	char up, down, left, right;
 	bool quit;
 	Player * p1;
+	Ball * b1;
 public:
 	game_manager(int w, int h) {	//constructor
 		quit = false;
 		up = 'w', down = 's', left = 'a', right = 'd';
 		width = w; height = h;
 		p1 = new Player(w / 2, h / 2);	//set player in the middle
+		b1 = new Ball(w / 2 + 1, h / 2);
 	}
 
 	~game_manager()
@@ -90,15 +99,19 @@ public:
 			for (int j = 0; j < width; j++)	// ROW
 			{
 				int playerx = p1->getX();
-				int playery = p1->getY();	//Get location of all objects
+				int playery = p1->getY();	//Get location of all objects (player)
+				int ballx = b1->getX();
+				int bally = b1->getY();		//Get local of ball
 
 				if (j == 0)	//draw the wall part at the start of every row
 					cout << "\xB2";	//hex character code
 
 				//Draw player location
 				if (playerx == j && playery == i)
-					cout << "O"; //player
-
+					cout << "\xFE"; //player
+				else if (ballx == j && bally == i)
+					cout << "O"; //ball
+				
 				else
 					cout << " ";
 
@@ -114,27 +127,57 @@ public:
 	}
 
 	void Input() {
-		p1->move_player();
-
 		int playerx = p1->getX();
 		int playery = p1->getY();	//Get location of all objects
+		int ballx = b1->getX();
+		int bally = b1->getY();
 
 		if (_kbhit())	//get user input
 		{
-			if (GetAsyncKeyState(87))	//ascii W checking asynchorously for Key state
-				if (playery > 0)
-					p1->moveUp();
-			if (GetAsyncKeyState(83))	//	S
-				if (playery < height -1)
-					p1->moveDown();
-			if (GetAsyncKeyState(65))	//	A
-				if (playerx > 0)
-					p1->moveLeft();
-			if (GetAsyncKeyState(68))	//	D
-				if (playery < width - 1)
-					p1->moveRight();
-			if (GetAsyncKeyState(82))	// R
+			if (GetAsyncKeyState(87)) {	//ascii W checking asynchorously for Key state
+				if ((playery > 0) && (bally > 0)) {
+					p1->moveUp(); b1->setY(p1->getY() - 1); b1->setX(p1->getX());	//move player and ball
+				}
+			}
+			if (GetAsyncKeyState(83)) {	//	S
+				if ((playery < height - 1) && (bally < height - 1)) {
+					p1->moveDown();	b1->setY(p1->getY() + 1); b1->setX(p1->getX());
+				}
+			}
+			if (GetAsyncKeyState(65)) {	//	A
+				if ((playerx > 0) && (ballx > 0)) {
+					p1->moveLeft();	b1->setX(p1->getX() - 1); b1->setY(p1->getY());
+				}
+			}
+			if (GetAsyncKeyState(68)) {	//	D
+				if ((playerx < width - 1) && (ballx < width - 1)) {
+					p1->moveRight(); b1->setX(p1->getX() + 1); b1->setY(p1->getY());
+				}
+			}
+			if (GetAsyncKeyState(87) && GetAsyncKeyState(68)) {	//W&D	sticky ball
+				if ((bally > 0) && (ballx < width - 1)) {
+					b1->setY(p1->getY() - 1); b1->setX(p1->getX() + 1);
+				}
+			}
+			else if (GetAsyncKeyState(87) && GetAsyncKeyState(65)) {	//W&A
+				if ((bally > 0) && (ballx > 0)) {
+					b1->setY(p1->getY() - 1); b1->setX(p1->getX() - 1);
+				}
+			}
+			else if (GetAsyncKeyState(83) && GetAsyncKeyState(65)) {	//S&A
+				if ((bally < height - 1) && (ballx > 0)) {
+					b1->setY(p1->getY() + 1); b1->setX(p1->getX() - 1);
+				}
+			}
+			else if (GetAsyncKeyState(83) && GetAsyncKeyState(68)) {	//S&D
+				if ((bally < height - 1) && (ballx < width - 1)) {
+					b1->setY(p1->getY() + 1); b1->setX(p1->getX() + 1);
+				}
+			}
+			if (GetAsyncKeyState(82)) {	// R
 				p1->Reset();
+				b1->Reset();
+			}
 			if (GetAsyncKeyState(81))	//ascii Q quit the game with q
 				quit = true;
 			
@@ -161,7 +204,7 @@ public:
 
 int main() {
 
-	game_manager g(40, 20);
+	game_manager g(70, 20);
 	g.Run();
 	return 0;
 }
