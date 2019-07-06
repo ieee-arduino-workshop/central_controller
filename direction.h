376c8f49562771bcfca294cb2b0db51e8dade6d8
Anthony <blacksunflowakuttaspam@hotmail.com>
2019-07-06 16:01:48 +1000

    moved direction enum to it's own file

---
+++ direction.h
@@ -0,0 +1,16 @@
+#ifndef DIRECTION_H
+#define DIRECTION_H
+
+enum eDir {
+    STOP = 0,
+    LEFT,
+    UPLEFT,
+    DOWNLEFT,
+    RIGHT,
+    UPRIGHT,
+    DOWNRIGHT,
+    UP,
+    DOWN
+};
+
+#endif
